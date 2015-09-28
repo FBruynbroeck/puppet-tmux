f175d57fb710e8ce6b155cbcc68c13f054f32587
FBruynbroeck <francois@affinitic.be>
2015-09-28 20:35:21 +0200

    Fix typo autostart

--- manifests/autostart.pp
+++ manifests/autostart.pp
@@ -4,7 +4,7 @@ define tmux::autostart(
 ) {
   if $user == 'root' { $home = '/root' } else { $home = "${tmux::params::home}/${user}" }
   if $user {
-    file_line { "${user}-autoupdate":
+    file_line { "${user}-autostart":
       path    => "${home}/.zshrc",
       line    => "export ZSH_TMUX_AUTOSTART=${autostart}",
       match   => 'export ZSH_TMUX_AUTOSTART='
